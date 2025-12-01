9074d804f01b8a60db046689fbbdd36edd6af39d
Jamescog <jamescog72@gmail.com>
2025-12-02 01:02:13 +0300

    feat: part-2

--- day1/main.go
+++ day1/main.go
@@ -25,11 +25,68 @@ func getNumber(turn string) int {
 
 }
 
-func main() {
-	file, err := os.Open("test.txt")
-	zerocount := 0
+func countZeroCross(current, turn int) int {
+	count := 0
+	if turn > 0 {
+		count = (current + turn) / 100
+	} else if turn < 0 {
+		val := -(current + turn)
+		term1 := 0
+		if val >= 0 {
+			term1 = val / 100
+		} else {
+			term1 = (val - 99) / 100
+		}
+
+		term2 := 0
+		if current > 0 {
+			term2 = -1
+		}
+
+		count = term1 - term2
+	}
+	return count
+}
+
+func partOne(inputs []int) int {
 
 	current := 50
+	zerocount := 0
+
+	for _, turn := range inputs {
+		current = (current + turn) % 100
+		if current < 0 {
+			current += 100
+		}
+
+		if current == 0 {
+			zerocount++
+		}
+	}
+
+	return zerocount
+}
+
+func partTwo(inputs []int) int {
+	current := 50
+	zerocount := 0
+
+	for _, turn := range inputs {
+		zerocount += countZeroCross(current, turn)
+		current = (current + turn) % 100
+		if current < 0 {
+			current += 100
+		}
+	}
+
+	fmt.Println("Zero crossed:", zerocount)
+	return zerocount
+}
+
+func main() {
+	file, err := os.Open("test.txt")
+
+	var inputs []int
 
 	if err != nil {
 		fmt.Println("Error Reading file")
@@ -40,15 +97,10 @@ func main() {
 
 	for scanner.Scan() {
 		turn := getNumber(scanner.Text())
-		current = (current + turn) % 100
-		if current < 0 {
-			current += 100
-		}
-		if current == 0 {
-			zerocount++
-		}
+		inputs = append(inputs, turn)
 	}
 
-	fmt.Println("The password is: ", zerocount)
+	fmt.Println("Anser for Part1: ", partOne(inputs))
+	fmt.Println("Anser for Part2: ", partTwo(inputs))
 
 }
